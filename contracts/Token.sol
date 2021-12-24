bfcb58af3b2ee059f3f8704ac6a94da1a13d570d
ludo loops <ludoloops@protonmail.com>
2021-12-24 03:42:55 +0100

    Feat: make token

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,17 +4,19 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Token {
-    string public name = "LudoLoops Token";
-    string public symbol = "LLT";
+    string public name = "Nader Dabit Token";
+    string public symbol = "NDT";
     uint256 public totalSupply = 1000000;
+    address public owner;
     mapping(address => uint256) balances;
 
     constructor() {
         balances[msg.sender] = totalSupply;
+        owner = msg.sender;
     }
 
     function transfer(address to, uint256 amount) external {
-        require(balances[msg.sender] >= amount, "Not enough Tokens");
+        require(balances[msg.sender] >= amount, "Not enough tokens");
         balances[msg.sender] -= amount;
         balances[to] += amount;
     }
